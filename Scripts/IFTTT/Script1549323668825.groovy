26da2323f54378861caaa0b1e428c09d6a879ba2
tarun <tarun@10.0.2.15>
2019-02-04 18:00:38 -0800

    Ifttt internal data comit

--- Scripts/IFTTT/Script1549323668825.groovy
+++ Scripts/IFTTT/Script1549323668825.groovy
@@ -1,7 +1,3 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
@@ -26,6 +22,12 @@ import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium as WebDriverB
 import static org.junit.Assert.*
 import java.util.regex.Pattern as Pattern
 import static org.apache.commons.lang3.StringUtils.join
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('https://accounts.google.com')
 
@@ -56,6 +58,7 @@ selenium.type('name=password', 'George@321')
 WebUI.click(findTestObject('Page_Gmail/Page_Gmail/span_Next'))
 
 WebUI.delay(5)
+for (def index : (1..3)) {
 
 selenium.open('https://ifttt.com/login?wp_=1')
 
@@ -66,35 +69,88 @@ selenium.click('link=Continue with Google')
 WebUI.click(findTestObject('Object Repository/Page_Sign in to your IFTTT account/a_Continue with Google'), FailureHandling.CONTINUE_ON_FAILURE)
 
 WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Discover IFTTT and Applets - I/span_georgejohn7866'))
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Discover IFTTT and Applets - I/a_New Applet'))
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/a_this'))
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/img'))
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/span_This Trigger fires every'))
-WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_httpcirilee.tumblr.co'), 'http://livefastdyung.tumblr.com/rss')
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/input_visit the help page_btn'))
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/span_'))
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/img_1'))
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/span_Create a photo post'))
-WebUI.delay(5)
-WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_Photo caption_fieldsc'), '')
-WebUI.delay(5)
-WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_a hrefhttpsamzn.to2CH'), '<a href="https://amzn.to/2CH5dsg">Buy Best Women\'s Intimates</a>')
-WebUI.delay(5)
-WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_Tags_fieldstags'), '')
-WebUI.delay(5)
-WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_foodcakedessertpastri'), 'food,cake,dessert,pastries,intersectional feminism,feminist,my hero academia,all might,lingerie,babe,sexy,babes,kakagai,halloween,horror,gold,gold flakes,nutrition,chocolate,cute,fitness,athletic,summer,fruit by the foot,fruit snacks')
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/input_Publish_btn btn-on-color'))
-WebUI.delay(5)
-WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/input_works with_btn btn-prima'))
-WebUI.delay(5)
+
+
+	WebUI.click(findTestObject('Object Repository/Page_Discover IFTTT and Applets - I/span_georgejohn7866'))
+	
+	WebUI.delay(5)
+	
+	
+	WebUI.click(findTestObject('Object Repository/Page_Discover IFTTT and Applets - I/a_New Applet'))
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/a_this'))
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/img'))
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/span_This Trigger fires every'))
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_httpcirilee.tumblr.co'), findTestData(
+			'rss').getValue(1, index))
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/input_visit the help page_btn'))
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/span_'))
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/img_1'))
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/span_Create a photo post'))
+	
+	WebUI.delay(5)
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_Photo caption_fieldsc'), '')
+	
+	WebUI.delay(5)
+	
+	WebUI.sendKeys(findTestObject('Page_Make an Applet - IFTTT/textarea_a hrefhttpsamzn.to2CH'), Keys.chord(Keys.CONTROL, 'a'))
+	
+	WebUI.delay(5)
+	
+	WebUI.sendKeys(findTestObject('Page_Make an Applet - IFTTT/textarea_a hrefhttpsamzn.to2CH'), Keys.chord(Keys.DELETE))
+	
+	WebUI.delay(5)
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_a hrefhttpsamzn.to2CH'), '<a href="https://amzn.to/2CH5dsg">Buy Best Women\'s Intimates</a>')
+	
+	WebUI.delay(5)
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_Tags_fieldstags'), '')
+	
+	WebUI.delay(5)
+	
+	WebUI.sendKeys(findTestObject('Page_Make an Applet - IFTTT/textarea_foodcakedessertpastri'), Keys.chord(Keys.CONTROL, 'a'))
+	
+	WebUI.delay(5)
+	
+	WebUI.sendKeys(findTestObject('Page_Make an Applet - IFTTT/textarea_foodcakedessertpastri'), Keys.chord(Keys.DELETE))
+	
+	WebUI.delay(5)
+	
+	WebUI.setText(findTestObject('Object Repository/Page_Make an Applet - IFTTT/textarea_foodcakedessertpastri'), 'food,cake,dessert,pastries,intersectional feminism,feminist,my hero academia,all might,lingerie,babe,sexy,babes,kakagai,halloween,horror,gold,gold flakes,nutrition,chocolate,cute,fitness,athletic,summer,fruit by the foot,fruit snacks')
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/input_Publish_btn btn-on-color'))
+	
+	WebUI.delay(5)
+	
+	WebUI.click(findTestObject('Object Repository/Page_Make an Applet - IFTTT/input_works with_btn btn-prima'))
+	
+	WebUI.delay(5)
+}
+
+
 
